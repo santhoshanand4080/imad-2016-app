44e88ee85687adbaf749db97c3c5f55c18664079
santhoshanand4080 <san.anand.n@gmail.com>
2016-11-26 14:07:23 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pg= require('pg');
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -21,6 +23,33 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var pg = new pg.Pool('config');
+
+var config = {
+  user: 'santhoshanand4080', //env var: PGUSER
+  database: 'santhoshanand4080', //env var: PGDATABASE
+  password:PROCESS.EMV.DB_PASSWORD, //env var: PGPASSWORD
+  host: 'db.imad.hasura-app.io', // Server hosting the postgres database
+  port: 5432, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+
+app.get('/santhoshanand4080', function(req,res)
+{
+
+pg.query(' select * from TBResume',function(req,res){
+    
+    if(err)
+    {
+        res.status(500).send(err.tostring);
+    }
+    }
+}
+})
+
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
