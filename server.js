599f2eaeff8c5af2ac3fdac09f678d5e8389a173
santhoshanand4080 <san.anand.n@gmail.com>
2016-11-27 14:53:23 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/santhoshanand4080', function(req,res)
 pool.query('select * from TBResume',function(err,result){
     if(err)
     {
-        res.status(500).send(err.tostring());
+        res.status(500).send(err);
     }
     else
     {
