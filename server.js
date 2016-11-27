1ef57c98e40b61c0aacd9f729d97b5ede2fa26d9
santhoshanand4080 <san.anand.n@gmail.com>
2016-11-27 14:57:05 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ var config = {
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
-
 var pool = new Pool(config);
-
 var app = express();
 app.use(morgan('combined'));
 
